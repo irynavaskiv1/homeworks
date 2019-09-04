5fde36d65c7dff8ea0001d0c2b74a54bfed4abe5
Iryna <irynavaskiv875@gmail.com>
2019-09-04 15:13:39 +0300

    first and second tasks

---
+++ homework_1.py
@@ -0,0 +1,25 @@
+# task1
+variable_a = int(input('Please, enter a variable '))
+variable_b = int(input('Please, enter b variable '))
+
+plus = variable_a + variable_b
+minus = variable_a - variable_b
+multiply = variable_a * variable_b
+share = variable_a / variable_b
+
+print('a+b =', plus)
+print('a-b =', minus)
+print('a*b =', multiply)
+print('a/b =', share)
+
+# task2
+print('What is your name ?')
+x = input()
+print('Hello, ' + x)
+
+y = input('How old are you ?')
+print('You age is ' + y)
+
+print('Where do you live?')
+z = input()
+print('You live in ' + z)
