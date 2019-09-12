4a71810d03cc1389bf597993764a7786f7df87c5
Iryna Vaskiv <>
2019-09-12 11:04:07 +0300

    update homework 1.1

---
+++ homework_1/homework_1.1.py
@@ -0,0 +1,13 @@
+# task1
+variable_a = int(input('Please, enter a variable '))
+variable_b = int(input('Please, enter b variable '))
+
+plus = variable_a + variable_b
+minus = variable_a - variable_b
+multiply = variable_a * variable_b
+share = variable_a / variable_b
+
+print('a+b =', plus)
+print('a-b =', minus)
+print('a*b =', multiply)
+print('a/b =', share)
