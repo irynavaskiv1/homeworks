8221578b0ce7faf3c25718337ddce1184e4955d7
Iryna Vaskiv <>
2019-09-23 14:31:33 +0300

    finish 5 hometask

---
+++ homework_5/homework_5.5.py
@@ -0,0 +1,3 @@
+def count_sheeps(arrayOfSheeps):
+    """Counting sheep..."""
+    return arrayOfSheeps.count(True)
---
+++ homework_5/homework_5.6.py
@@ -0,0 +1,3 @@
+def correct_tail(body, tail):
+    """Is this my tail?"""
+    return body[-1] == tail
