cb1a7c00a92f1502af4cfa0b8f05e71a6d1d12fe
Iryna Vaskiv <>
2019-09-24 16:34:31 +0300

    second step of 6 hometask

--- homework_6/homework_6.1.py
+++ homework_6/homework_6.1.py
@@ -2,5 +2,6 @@ import math
 
 
 def distance(x1, y1, x2, y2):
+    """"Find The Distance Between Two Points"""
     distanc = (x2 - x1)**2 + (y2 - y1)**2
     return round(math.sqrt(distanc), 2)
--- homework_6/homework_6.3.py
+++ homework_6/homework_6.3.py
@@ -1,4 +1,4 @@
 def filter_words(st):
-    if taken ==
-    taken ="WOW this is REALLY amazing"
-    taken.
+    """No yelling!"""
+    st = "HELLO world!"
+    return st.capitalize()
