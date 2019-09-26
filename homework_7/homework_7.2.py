e1b7a2caa35647a4c689e01e2918729c10ac1750
Iryna Vaskiv <>
2019-09-26 18:21:58 +0300

    homework 7 first update

---
+++ homework_7/homework_7.2.py
@@ -0,0 +1,9 @@
+def reverse_list(l):
+    """Reverse List Order"""
+    l = [1, 2, 4, 5, 7, 88]
+    print(list(reversed(l)))
+
+
+def reverse_list_second(l):
+    """Reverse List Order"""
+    return l[::-1]
