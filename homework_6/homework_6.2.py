198515bea438161a15bb570f6a6887c4fbf07ed6
Iryna Vaskiv <>
2019-09-28 13:08:45 +0300

    update 6 homework

---
+++ homework_6/homework_6.2.py
@@ -0,0 +1,4 @@
+def make_move(sticks):
+    """21 Sticks"""
+    r = sticks % 4
+    return r
---
+++ homework_6/homework_6.4.py
@@ -0,0 +1,6 @@
+def filter_words(st):
+    """Unfinished Loop - Bug Fixing #1"""
+    my_list = st.split()
+    new_list = ' '.join(my_list)
+    new_list.lower()
+    return new_list.capitalize()
