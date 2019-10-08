d669f6a24787bc08e6b85d192980d36ff4511d5f
Iryna Vaskiv <>
2019-10-08 14:34:35 +0300

    update 6 classwork

---
+++ Classwork/class_work_6.py
@@ -0,0 +1,54 @@
+# Створити батьківський клас Figure з методами init: ініціалізується колір,
+# get_color: повертає колір фігури,
+# info: надає інформацію про фігуру та колір,
+# від якого наслідуються такі класи як Rectangle, Square, які мають інформацію
+# про ширину, висоту фігури, метод square, який знаходить площу фігури.
+#
+
+
+class Figure:
+
+    def __init__(self, number_of_sides):
+        self.number_of_sides = number_of_sides
+        self.sides = [0 for i in range(number_of_sides)]
+
+    def enter_the_sides(self):
+        self.sides = [int(input("Enter side " + str(i + 1) + " : "))
+                      for i in range(self.number_of_sides)]
+
+    def show_side(self):
+        for i in range(self.number_of_sides):
+            print("Side", i+1, "is", self.sides[i])
+
+
+class Rectangle(Figure):
+
+    def __init__(self):
+        super().__init__(2)
+
+    def square(self):
+        width, height = self.sides
+        square = width * height
+        print('The square of your figure is ', square)
+
+
+class Square(Figure):
+
+    def __init__(self):
+        super().__init__(1)
+
+    def square(self):
+        side = self.sides
+        square = side * 2
+        print('The square of your figure is ', square)
+
+
+r = Rectangle()
+r.enter_the_sides()
+r.show_side()
+r.square()
+
+s = Square()
+s.enter_the_sides()
+s.show_side()
+s.square()
