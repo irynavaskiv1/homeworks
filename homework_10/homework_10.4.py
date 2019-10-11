bb98799056afba666f7abe86290323433991d0bf
Iryna Vaskiv <>
2019-10-11 14:19:24 +0300

    update 10 homework

--- homework_10/homework_10.4.py
+++ homework_10/homework_10.4.py
@@ -1,2 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.info = self.name + "'s age is " + str(self.age)
+
+    def get_information(self):
+        print(self.info)
 
 
+p1 = Person('Iryna', 23)
+p1.get_information()
